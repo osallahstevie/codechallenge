88f709ec5633ab649cc7649323a0ba279f15a314
osallahstevie <osallahstevie@gmail.com>
2022-11-26 13:23:36 +0300

    declared a variable

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Function for calculating grades
+var students = [['Dennis', 80], ['Ben', 77], ['Joy', 88], ['Tancy', 95], ['Tom', 68]];
+var Avgmarks = 0;
 
-const calculate = () => {}
+
+
+    
+
