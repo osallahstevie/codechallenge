464cfc07072eb9c32653e3d1fabd430fb0e9339e
osallahstevie <osallahstevie@gmail.com>
2022-11-26 14:00:59 +0300

    Grades on student based on percentage

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ for (var i=0; i < students.length; i++){
     Avgmarks += students[i][1];
     var avg =(Avgmarks/students.length);
 }
+console.log("Average grade: " + (Avgmarks)/students.length);
+
+if (avg > 79) {
+    console.log("Grade : A");
+    
+}else if (avg > 60) {
+    console.log("Grade : B")
+}else if (avg > 49) {
+    console.log("Grade : C")
+}else if (avg > 40){
+    console.log("Grade : D")
+}else if (avg > 40){
+    console.log("Grade : E")
+}
 
 
 
