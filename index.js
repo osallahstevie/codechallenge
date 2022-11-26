f1478563b9e14b135c50d5702f3232b59ef32e69
osallahstevie <osallahstevie@gmail.com>
2022-11-26 13:29:34 +0300

    declared a variable to calculate average marks

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 var students = [['Dennis', 80], ['Ben', 77], ['Joy', 88], ['Tancy', 95], ['Tom', 68]];
 var Avgmarks = 0;
 
+for (var i=0; i < students.length; i++){
+    Avgmarks += students[i][1];
+    var avg =(Avgmarks/students.length);
+}
+
 
 
     
